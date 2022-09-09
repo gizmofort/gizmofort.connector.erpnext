ee782f1211c5053a150cc6475c798d46ea9ca660
Jerrill <jerrill@gmail.com>
2022-09-09 01:16:32 -0400

    fix glaring typo in block comment headers of generated files (#2)
    Co-authored-by: Jerrill Johnson <jerrill@lexgetdigital.com>

--- Tests/GizmoFort.Connector.ERPNext.Tests/Constants/TestConstants.cs
+++ Tests/GizmoFort.Connector.ERPNext.Tests/Constants/TestConstants.cs
@@ -2,8 +2,12 @@
 {
     public static class TestConstants
     {
-        public const string TEST_DOMAIN = "https://your.erpnext.com";
-        public const string TEST_USERNAME = "username";
-        public const string TEST_PASSWORD = "password";
+        //public const string TEST_DOMAIN = "https://your.erpnext.com";
+        //public const string TEST_USERNAME = "username";
+        //public const string TEST_PASSWORD = "password";
+
+        public const string TEST_DOMAIN = "http://localhost:8080/";
+        public const string TEST_USERNAME = "superadmin@lexgetdigital.com";
+        public const string TEST_PASSWORD = "4f!MmFZYoxum98@JlM85";
     }
 }
